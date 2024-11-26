968961d59dec6e19cab0f98b084414a58e4395c5
chanamoth <mario.rojas.chanamoth@gmail.com>
2024-11-26 17:46:50 -0500

    Se corrige para correr en laragon

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Mostrar errores
 =============================================*/
 ini_set('display_errors', 1);
 ini_set("log_errors", 1);
-ini_set("error_log",  "D:/xampp/htdocs/studiomutante/mathapp/php_error_log");
+ini_set("error_log",  "C:/laragon/www/mathapp/php_error_log");
 
 /*=============================================
 Requerimientos
